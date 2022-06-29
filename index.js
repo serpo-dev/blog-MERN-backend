e1a3633019dd3b85abce1bbfef3b92db8dc6a7c8
Sergey <sergey.potapov.2002@mail.ru>
2022-06-29 14:48:24 +0500

    did authorization/login

--- index.js
+++ index.js
@@ -18,6 +18,48 @@ mongoose
 const app = express();
 app.use(express.json()); // need to read json files in requests
 
+app.post('/auth/login', async (req, res) => {
+    try {
+        const user = await UserModel.findOne({ email: req.body.email });
+        if (!user) {
+            return req
+                .status(404)
+                .json({ message: 'wrong login or password' });
+        };
+
+        // compare entered and database passwords
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+        if (!isValidPass) {
+            return req
+                .status(404)
+                .jsom({ message: 'wrong login or password' });
+        };
+
+        const token = jwt.sign(
+            {
+                _id: user._id
+            },
+            'secret123',
+            {
+                expiresIn: '30d'
+            }
+        );
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            ...userData,
+            token
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+                message: 'Server can\'t authorize you'
+            });
+    };
+});
 
 app.post('/auth/registration', registerValidation, async (req, res) => {
     try {
@@ -28,26 +70,39 @@ app.post('/auth/registration', registerValidation, async (req, res) => {
 
         const password = req.body.password;
         const salt = await bcrypt.genSalt(10);
-        const passwordHash = await bcrypt.hash(password, salt);
+        const hash = await bcrypt.hash(password, salt);
 
         const doc = new UserModel({
             email: req.body.email,
             fullName: req.body.fullName,
             avatarUrl: req.body.avatarUrl,
-            passwordHash
+            passwordHash: hash
         });
 
         const user = await doc.save();
 
-        const token = jwt.sign({
-            _id: user._id
-        },
+        const token = jwt.sign(
+            {
+                _id: user._id
+            },
             'secret123',
             {
                 expiresIn: '30d'
-            })
+            }
+        );
 
-        res.json(user);
+        // destructuration to hide 'passwordHash' at a response
+        const {
+            passwordHash,
+            ...userData
+        } = user._doc;
+
+        // response
+        res.json({
+            ...userData,
+            token
+        }
+        );
 
     } catch (err) {
         console.log(err)
