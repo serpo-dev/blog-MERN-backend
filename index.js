185b08394ce1c8ab399d5f6932e9e80e8a6c6a42
Sergey <sergey.potapov.2002@mail.ru>
2022-06-29 10:50:02 +0500

    connect express-validattor

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 import express from 'express';
+import { validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+
+import registerValidation from './validations/auth.js'
+
+
+mongoose
+    .connect('mongodb+srv://yphwd:990615@cluster0.zdaa7kb.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => { console.log('BD is OK') })
+    .catch((err) => { console.log('DB is not avaliable. Error type: ', err) })
+
+
 
 const app = express();
+app.use(express.json()); // need to read json files in requests
 
-app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-});
+app.post('/auth/registration', registerValidation, (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json(errors.array());
+    };
 
-app.post('/auth/login', (req, res) => {
     res.json({
-        status: 'success'
+        success: true
     });
 });
+
 
 app.listen(4444, (err) => {
     if (err) {
