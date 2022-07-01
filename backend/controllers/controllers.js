cf9ec8d0db89016822ef4d3ed5dd0fc4e5fdbc50
Sergey <sergey.potapov.2002@mail.ru>
2022-07-01 09:04:40 +0500

    Delete backend directory

--- backend/controllers/controllers.js
+++
@@ -1,2 +0,0 @@
-export * as PostController from './PostController.js';
-export * as UserController from './UserController.js';
--- backend/validations/posts.js
+++
@@ -1,9 +0,0 @@
-//check if there's  information in the body and validate it
-import { body } from 'express-validator';
-
-
-export const postCreateOrUpdateValidation = [
-    body('title', 'Length need to be at least 1 charachter').isLength({ min: 1 }).isString(),
-    body('text', 'Length need to be at least 1 charachter').isLength({ min: 1 }).isString(),
-    body('imageUrl', 'Your link does not look like any true link').optional().isString()
-];
